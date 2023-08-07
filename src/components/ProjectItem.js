711938ab2409f7ba467746b212cf791921157671
sheldon <sheldonmwangi254@gmail.com>
2023-08-07 08:04:49 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techList = Object.entries(technologies).map(([key, value]) => {
+    return (
+      <span key={key}> {value.toString()} </span>
+    );
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {technologies.map((technology) => {
-          return <span key={technology}>{technology}</span>;
-        })}
+      <div className="technologies" >
+        {techList}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,19 +2,22 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">
-        {projects.map((project) => (
-          <ProjectItem
-            key={project.id}
-            name={project.name}
-            about={project.about}
-            technologies={project.technologies}
-          />
-        ))}
+        {
+          projects.map((e) => {
+            return (
+              <ProjectItem
+                key={e.id.toString()}
+                name={e.name}
+                about={e.about}
+                technologies={e.technologies} />
+            )
+          }
+          )
+        }
       </div>
     </div>
   );
